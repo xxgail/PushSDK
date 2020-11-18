baf481d6235861d20e865e6220c9b33871c81b20
Gail <xxxggggail@gmail.com>
2020-11-18 15:56:42 +0800

    debug reflect.TypeOf(*P)

--- send.go
+++ send.go
@@ -3,8 +3,6 @@ package PushSDK
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"reflect"
 )
 
 type Send struct {
@@ -46,12 +44,11 @@ func setHWParam(str string) (*HW, error) {
 	var err error
 	var param *HW
 	_ = json.Unmarshal([]byte(str), &param)
-	t := reflect.TypeOf(param)
-	v := reflect.ValueOf(param)
-	for k := 0; k < t.NumField(); k++ {
-		if v.Field(k).Interface() == nil {
-			err = errors.New(t.Field(k).Name + "不能为空")
-		}
+	if param.AppId == "" {
+		return param, errors.New("AppId" + "can not be empty")
+	}
+	if param.ClientSecret == "" {
+		return param, errors.New("ClientSecret" + "can not be empty")
 	}
 	return param, err
 }
@@ -82,12 +79,11 @@ func setMIParam(str string) (*MI, error) {
 	var err error
 	var param *MI
 	_ = json.Unmarshal([]byte(str), &param)
-	t := reflect.TypeOf(param)
-	v := reflect.ValueOf(param)
-	for k := 0; k < t.NumField(); k++ {
-		if v.Field(k).Interface() == nil {
-			err = errors.New(t.Field(k).Name + "不能为空")
-		}
+	if param.AppSecret == "" {
+		return param, errors.New("AppSecret" + "can not be empty")
+	}
+	if param.RestrictedPackageName == "" {
+		return param, errors.New("RestrictedPackageName" + "can not be empty")
 	}
 	return param, err
 }
@@ -96,12 +92,11 @@ func setMZParam(str string) (*MZ, error) {
 	var err error
 	var param *MZ
 	_ = json.Unmarshal([]byte(str), &param)
-	t := reflect.TypeOf(param)
-	v := reflect.ValueOf(param)
-	for k := 0; k < t.NumField(); k++ {
-		if v.Field(k).Interface() == nil {
-			err = errors.New(t.Field(k).Name + "不能为空")
-		}
+	if param.AppSecret == "" {
+		return param, errors.New("AppSecret" + "can not be empty")
+	}
+	if param.AppId == "" {
+		return param, errors.New("AppId" + "can not be empty")
 	}
 	return param, err
 }
@@ -110,12 +105,11 @@ func setOPPOParam(str string) (*OPPO, error) {
 	var err error
 	var param *OPPO
 	_ = json.Unmarshal([]byte(str), &param)
-	t := reflect.TypeOf(param)
-	v := reflect.ValueOf(param)
-	for k := 0; k < t.NumField(); k++ {
-		if v.Field(k).Interface() == nil {
-			err = errors.New(t.Field(k).Name + "不能为空")
-		}
+	if param.AppKey == "" {
+		return param, errors.New("AppKey" + "can not be empty")
+	}
+	if param.MasterSecret == "" {
+		return param, errors.New("MasterSecret" + "can not be empty")
 	}
 	return param, err
 }
@@ -140,31 +134,29 @@ func setVIVOParam(str string) (*VIVO, error) {
 }
 
 func (s *Send) SendMessage(message *MessageBody) (*Response, error) {
-	fmt.Println("ssssss", s)
-	fmt.Println("mmmmmm", message)
 	var (
 		err error
 	)
 	if message.Title == "" {
-		return &Response{}, errors.New("标题不能为空")
+		return &Response{}, errors.New("title can not be empty")
 	}
 	if message.Desc == "" {
-		return &Response{}, errors.New("内容不能为空")
+		return &Response{}, errors.New("content can not be empty")
 	}
 	if message.ClickType != "app" && message.ClickContent == "" {
-		return &Response{}, errors.New("点击内容不能为空")
+		return &Response{}, errors.New("ClickContent can not be empty")
 	}
 	channel := s.Channel
 	if channel == "" {
-		return &Response{}, errors.New("发送渠道不能为空")
+		return &Response{}, errors.New("channel can not be empty")
 	}
 	pushId := s.PushId
 	if len(pushId) == 0 {
-		return &Response{}, errors.New("发送用户不能为空")
+		return &Response{}, errors.New("pushId can not be empty")
 	}
 	plat := s.PlatForm
 	if plat == "" {
-		return &Response{}, errors.New("请设置相应发送渠道参数")
+		return &Response{}, errors.New("please set platform param of channel")
 	}
 
 	var mc MobileChannel
